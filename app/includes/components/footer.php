6dc682677c143725a89b7d2c54a07d0a3b5e8513
HenriqueCoelho1 <alleson_henrique@hotmail.com>
2021-05-12 17:46:48 -0300

    Making Our Files Reuseable

---
+++ app/includes/components/footer.php
@@ -0,0 +1,18 @@
+<footer>
+            <div class="row">
+                <div class="col-lg-12">
+                    <p>Copyright &copy; HC Blog 2021</p>
+                </div>
+                
+            </div>
+            
+        </footer>
+
+    </div>
+
+    <script src="js/jquery.js"></script>
+    <script src="js/bootstrap.min.js"></script>
+
+</body>
+
+</html>
