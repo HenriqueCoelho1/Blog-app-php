e9bed322471a3876299150d651dd2c9dd20f7714
HenriqueCoelho1 <alleson_henrique@hotmail.com>
2021-05-13 19:17:41 -0300

    Creating the posts table

--- database.sql
+++ database.sql
@@ -1,6 +1,7 @@
 -- drop database db;
 -- create database db;
 -- drop table user;
+-- drop table posts;
 
 use db;
 
@@ -8,17 +9,38 @@ CREATE TABLE user (
     id INTEGER NOT NULL AUTO_INCREMENT,
     username VARCHAR(40) NOT NULL,
     password VARCHAR(200) NOT NULL,
-    Primary key(id)
+    PRIMARY KEY(id)
 );
 
 CREATE TABLE category (
 	id INTEGER NOT NULL AUTO_INCREMENT,
     title VARCHAR(255) NOT NULL,
-    Primary key(id)
+    PRIMARY KEY(id)
+);
+
+CREATE TABLE post(
+    id INTEGER NOT NULL AUTO_INCREMENT,
+    title VARCHAR(255) NOT NULL,
+    author VARCHAR(255) NOT NULL,
+    dh_insert DATETIME NOT NULL,
+    image TEXT NOT NULL,
+    content TEXT NOT NULL,
+    tags VARCHAR(255) NOT NULL,
+    comment_count VARCHAR(255) NOT NULL,
+    status VARCHAR(255) DEFAULT 'Draft' NOT NULL,
+    category INTEGER NOT NULL, 
+    PRIMARY KEY(id),
+    FOREIGN KEY(category) REFERENCES category(id)
 );
 
 --SELECT * FROM `category`;
 --INSERT INTO category SET id = 2, title = 'Bootstrap';
+
+
+
+
+
+
 
 
 
