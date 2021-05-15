94a74c362537361c28c2625a2e059b9b2fd6eb38
HenriqueCoelho1 <alleson_henrique@hotmail.com>
2021-05-15 18:58:52 -0300

    Adding Categories to the sidebar

--- app/includes/components/sidebar.php
+++ app/includes/components/sidebar.php
@@ -21,40 +21,26 @@
                 <div class="well">
                     <h4>Blog Categories</h4>
                     <div class="row">
-                        <div class="col-lg-6">
-                            <ul class="list-unstyled">
-                                <li><a href="#">Category Name</a>
-                                </li>
-                                <li><a href="#">Category Name</a>
-                                </li>
-                                <li><a href="#">Category Name</a>
-                                </li>
-                                <li><a href="#">Category Name</a>
-                                </li>
-                            </ul>
+                        <div class="col-lg-12">
+                            <?php
+                            $query = "SELECT * FROM category LIMIT 3";
+                            $select_category_sidebar =  mysqli_query($connection, $query);
+
+                            while($row = mysqli_fetch_assoc($select_category_sidebar)){
+                                $category_title = $row['title'];
+                                echo "<li><a href='#'>{$category_title}</a></li>";
+
+                            }
+                            ?>
                         </div>
                         
-                        <div class="col-lg-6">
-                            <ul class="list-unstyled">
-                                <li><a href="#">Category Name</a>
-                                </li>
-                                <li><a href="#">Category Name</a>
-                                </li>
-                                <li><a href="#">Category Name</a>
-                                </li>
-                                <li><a href="#">Category Name</a>
-                                </li>
-                            </ul>
-                        </div>
+                        
 
                     </div>
                     
                 </div>
 
                 
-                <div class="well">
-                    <h4>Side Widget Well</h4>
-                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Inventore, perspiciatis adipisci accusamus laudantium odit aliquam repellat tempore quos aspernatur vero.</p>
-                </div>
+                <?php include "widget.php";?>
 
             </div>
