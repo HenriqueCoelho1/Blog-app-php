fd1777e62c20df221feca440dc70f99be00b9914
HenriqueCoelho1 <alleson_henrique@hotmail.com>
2021-05-18 00:42:25 -0300

    Creating a HTML table in admin to display a list of posts

---
+++ app/admin/posts.php
@@ -0,0 +1,52 @@
+<?php include "includes/components/admin_header.php";?>
+
+    <div id="wrapper"> 
+        <?php include "includes/components/admin_nav.php";?>
+        
+
+        <div id="page-wrapper">
+
+            <div class="container-fluid">
+
+                
+                <div class="row">
+                    <div class="col-lg-12">
+                        <h1 class="page-header">
+                            Welcome to Admin
+                            <small>Author</small>
+                        </h1>
+                        <table class="table table-border table-hover">
+                            <thead>
+                                <tr>
+                                    <th>Id</th>
+                                    <th>Author</th>
+                                    <th>Title</th>
+                                    <th>Category</th>
+                                    <th>Status</th>
+                                    <th>Image</th>
+                                    <th>Tags</th>
+                                    <th>Date</th>
+                                </tr>
+                            </thead>
+                            <tbody>
+                                <tr>
+                                    <th>aaa</th>
+                                    <th>1212</th>
+                                    <th>aaaaa</th>
+                                    <th>222222</th>
+                                    <th>33333</th>
+                                    <th>44444</th>
+                                    <th>5555555</th>
+                                    <th>666666666</th>
+                                </tr>
+                            </tbody>
+                        </table>
+                        
+                        
+                    </div>
+                </div>
+            </div>
+        </div>
+        
+
+        <?php include "includes/components/admin_footer.php";?>  
