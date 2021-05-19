2c2ed97a4ef400fa0e70fb592485f6db3590bfa6
HenriqueCoelho1 <alleson_henrique@hotmail.com>
2021-05-19 14:15:48 -0300

    Including Pages Based on Condition Technique

--- app/admin/posts.php
+++ app/admin/posts.php
@@ -15,27 +15,26 @@
                             Welcome to Admin
                             <small>Author</small>
                         </h1>
-                        <table class="table table-border table-hover">
-                            <thead>
-                                <tr>
-                                    <th>Id</th>
-                                    <th>Title</th>
-                                    <th>Author</th>
-                                    <th>Date</th>
-                                    <th>Image</th>
-                                    <th>Content</th>
-                                    <th>Tags</th>
-                                    <th>Comments</th>
-                                    <th>Status</th>
-                                    <th>Category</th>
-                                </tr>
-                            </thead>
-                            <tbody>
-                                <?php find_all_posts();?>
-                                    
-                                
-                            </tbody>
-                        </table>
+                        <?php
+                        if(isset($_GET['source'])){
+                            $source = $_GET['source'];
+                        }else {
+                            $source = '';
+                        }
+                        switch($source){
+                            case '33':
+                            echo "Hello World!";
+                            break;
+
+                            case '34':
+                            echo "Hello World!";
+                            break;
+
+                            default:
+                            include "includes/components/view_all_posts.php";
+                            break;
+                        }
+                        ?>
                         
                         
                     </div>
