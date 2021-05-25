203e557a5b7f1d98d602b6104b0157fdf47676de
HenriqueCoelho1 <alleson_henrique@hotmail.com>
2021-05-25 18:04:23 -0300

    Adding Users

--- app/admin/includes/components/add_user.php
+++ app/admin/includes/components/add_user.php
@@ -1,78 +1,67 @@
 <?php
-if(isset($_POST['create_post'])){
-    $post_title = $_POST['title'];
-    $post_author = $_POST['author'];
-    $post_category = $_POST['category'];
-    $post_status = $_POST['status'];
+if(isset($_POST['create_user'])){
 
-    $post_image = $_FILES['image']['name'];
-    $post_image_temp = $_FILES['image']['tmp_name'];
-
-    $post_tags = $_POST['tags'];
-    $post_content = $_POST['content'];
-    $post_dh_insert = date('dd-mmm-yyyy');
-
-
-    move_uploaded_file($post_image_temp, "../upload/$post_image");
+    $user_username = $_POST['username'];
+    $user_email = $_POST['email'];
+    $user_password = $_POST['password'];
+    $user_firstname = $_POST['firstname'];
+    $user_lastname = $_POST['lastname'];
+    $user_is_superuser = $_POST['is_superuser'];
+    $user_image = $_FILES['image']['name'];
+    $user_image_temp = $_FILES['image']['tmp_name'];
+    $user_dh_insert = date('dd-mm-yyyy');
+    $rand_salt = '123456abc';
 
 
-    $query = "INSERT INTO post(category, title, author, dh_insert, image, content, tags, status) VALUES ({$post_category}, '{$post_title}', '{$post_author}', now(), '{$post_image}', '{$post_content}', '{$post_tags}', '{$post_status}')";
+    move_uploaded_file($user_image_temp, "../upload/$user_image");
+
+
+    $query_user = "INSERT INTO user (username, email, password, firstname, lastname, is_superuser, image, dh_insert ,rand_salt) VALUES ('{$user_username}', '{$user_email}', '{$user_password}', '{$user_firstname}', '{$user_lastname}', {$user_is_superuser}, '{$user_image}', now(), '{$rand_salt}')";
     
-    $create_post_query = mysqli_query($connection, $query);
+    $create_user_query = mysqli_query($connection, $query_user);
 
 
-    confirm_query($create_post_query);
+    confirm_query($create_user_query);
     
 }
 ?>
 
 <form action="" method="post" enctype="multipart/form-data">
     <div class="form-group">
-        <label for="title">Post Title</label>
-        <input type="text" class="form-control" name="title" />
+        <label for="username">Username</label>
+        <input type="text" class="form-control" name="username" />
     </div>
     <div class="form-group">
-        <select name="category" id="">
-        <?php
-        $query_display_category = "SELECT * FROM category";
-        $select_categories = mysqli_query($connection, $query_display_category);
-
-        confirm_query($select_categories);
-
-        echo "<option value=''>Select</option>";
-        while($row = mysqli_fetch_assoc($select_categories)){
-            $id = $row['id'];
-            $title = $row['title'];
-
-            echo "<option value='{$id}'>{$title}</option>";
-        }
-
-        ?>
+        <label for="email">E-mail</label>
+        <input type="email" class="form-control" name="email" />
+    </div>
+    <div class="form-group">
+        <label for="password">Password</label>
+        <input type="password" class="form-control" name="password" />
+    </div>
+    <div class="form-group">
+        <label for="firstname">First Name</label>
+        <input type="text" class="form-control" name="firstname" placeholder="Your First Name" />
+    </div>
+    <div class="form-group">
+        <label for="lastname">Last Name</label>
+        <input type="text" class="form-control" name="lastname" placeholder="Your Last Name" />
+    </div>
+    <div class="form-group">
+        <!-- <label for="is_superuser">Super User</label>
+        <input type="number" class="form-control" name="is_superuser" placeholder="Super User" /> -->
+        <select name="is_superuser" id="">
+            <option value="">Select</option>
+            <option value="0">Is User</option>
+            <option value="1">Is Admin</option>
         </select>
-            
-    </div>
-    <div class="form-group">
-        <label for="author">Post Author</label>
-        <input type="text" class="form-control" name="author" />
-    </div>
-    <div class="form-group">
-        <label for="status">Post Status</label>
-        <input type="text" class="form-control" name="status" />
     </div>
     <div class="form-group">
         <label for="image">Post Image</label>
         <input type="file" name="image" />
     </div>
-    <div class="form-group">
-        <label for="tags">Post Tags</label>
-        <input type="text" class="form-control" name="tags" />
-    </div>
-    <div class="form-group">
-        <label for="post_content">Post Content</label>
-        <textarea class="form-control" name="content" cols="30" rows="10" placeholder="Post a content"></textarea>
-    </div>
 
     <div class="form-group">
-        <input class="btn btn-primary" type="submit" name="create_post" value="Publish Post" />
+        <input class="btn btn-primary" type="submit" name="create_user" value="Publish User" />
     </div>
 </form>
