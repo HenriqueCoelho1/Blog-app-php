8a21fcbb9a180790e04faac8e26c70c9ed54c870
HenriqueCoelho1 <alleson_henrique@hotmail.com>
2021-05-27 22:18:58 -0300

    Creating the sidebar login

--- app/includes/components/sidebar.php
+++ app/includes/components/sidebar.php
@@ -3,14 +3,17 @@
 
                 
                 <div class="well">
-                    <h4>Blog Search</h4>
-                    <form action="search.php" method="post">
+                    <h2 class="text-center">Login</h2>
+                    <form action="includes/components/login.php" method="post">
+                        <div class="form-group">
+                            <input name="username" type="text" class="form-control" placeholder="Enter Your Username" />
+                            
+                        </div>
                         <div class="input-group">
-                            <input name="search" type="text" class="form-control">
+                            <input name="password" type="password" class="form-control" placeholder="Enter Your Password" />
                             <span class="input-group-btn">
-                                <button name="submit" class="btn btn-default" type="submit">
-                                    <span class="glyphicon glyphicon-search"></span>
-                                </button>
+                                <button class="btn btn-primary" name="login" type="submit">Login</button>
+
                             </span>
                         </div>
                     </form>
