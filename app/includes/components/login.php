02d418ed881d8e4c60e41fe177659a1389e1a1fd
HenriqueCoelho1 <alleson_henrique@hotmail.com>
2021-05-30 16:54:03 -0300

    Fixing the login part 1

--- app/includes/components/login.php
+++ app/includes/components/login.php
@@ -14,6 +14,7 @@ if(isset($_POST["login"])){
         die("QUERY FAILED! " . mysqli_error($connection));
     }
 
+
     while($row = mysqli_fetch_array($select_user_query)){
         $db_user_id = $row['id'];
         $db_user_username = $row['username'];
@@ -22,14 +23,29 @@ if(isset($_POST["login"])){
         $db_user_lastname = $row['lastname'];
         $db_user_is_superuser = $row['is_superuser'];
 
-    }
+        // if($username !== $db_user_username && $password !== $db_user_password){
+        //     header("Location: index.php");
+        // }
+        // else if($username == $db_user_username && $password == $db_user_password){
+        //     header("Location: /admin");
+        // }
+        // else{
+        //     header("Location: index.php");
 
-    if($username !== $db_user_username && $password !== $db_user_password){
-        header("Location: ../../index.php");
+        // }
+
+        if($username === $db_user_username && $password === $db_user_password){
+            header("Location: /admin");
+        }
+        else{
+            header("Location: ../index.php");
+
+        }
+
     }
-    else if($username == $db_user_username && $password == $db_user_password){
-        header("Location: /admin");
-    }
+    
+
+    
 }
 
 
