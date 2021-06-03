f855449ce05e7bdcf4798caf3b1424208c3bd082
HenriqueCoelho1 <alleson_henrique@hotmail.com>
2021-06-03 20:36:27 -0300

    Fixind login CSS

--- app/includes/logout.inc.php
+++ app/includes/logout.inc.php
@@ -3,5 +3,5 @@ ob_start();
 session_start();
 session_unset();
 session_destroy();
-header("Location ../index.php");
+header("Location: ../index.php");
 exit();
--- app/login.php
+++ app/login.php
@@ -2,31 +2,48 @@
 <?php include "includes/header.php"; ?>
 <?php include "includes/nav.php";?>
 
-<h2>Sign In</h2>
-<form action="includes/login.inc.php" method="post">
-    <div class="form-group">
-        <label for="username">Username</label>
-        <input type="text" name="username" class="form-control" placeholder="Username Or Email" />
-    </div>
-    <div class="form-group">
-        <label for="password">Password</label>
-        <input type="password" name="password" class="form-control" placeholder="Your password" />
-    </div>
-    <div class="form-group">
-        <input class="btn btn-primary" type="submit" name="submit" />
-    </div>
+<section class="hero has-background-white-ter is-fullheight">
+    <div class="hero-body">
+        <div class="container">
+            <div class="columns is-centered">
+                <div class="is-5-tablet is-4-desktop is-3-widescreen">
+                    <form action="includes/login.inc.php" method="post">
+                        <h3 class="title is-3 is-centered">Login</h3>
+                        <div class="field p-1">    
+                                <label class="label" for="username">Name: </label>
+                                <div class="control">
+                                    <input class="input is-primary" type="text" placeholder="Your Name" name="username" value="" />
+                                </div>
+                        </div>
+                        <div class="field p-1">    
+                                <label class="label" for="password">Password: </label>
+                                <div class="control">
+                                    <input class="input is-primary" type="password" placeholder="Your Password" name="password" value="" />
+                                </div>
+                        </div>
 
-<?php
-if(isset($_GET["error"])){
-    if($_GET["error"] == "emptyinput"){
-        echo "<p>Fill in all fields!</p>";
-    }
-    else if($_GET["error"] == "wronglogin"){
-        echo "<p>Incorrect login information!</p>";
-    }
-}
-?>  
-</form>
+                        <div class="field p-1">
+                                <div class="control">
+                                    <button class="button is-primary" name="submit" value="">Login</button>
+                                </div>
+                        </div>
+
+                        <?php
+                        if(isset($_GET["error"])){
+                            if($_GET["error"] == "emptyinput"){
+                                echo "<p>Fill in all fields!</p>";
+                            }
+                            else if($_GET["error"] == "wronglogin"){
+                                echo "<p>Incorrect login information!</p>";
+                            }
+                        }
+                        ?>  
+                    </form>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
 
 
 
