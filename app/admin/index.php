6aba0ee3478582734b6636ebe74dd66be28f5886
HenriqueCoelho1 <alleson_henrique@hotmail.com>
2021-06-21 21:30:33 -0300

    Widgets

--- app/admin/index.php
+++ app/admin/index.php
@@ -16,36 +16,177 @@ if(isset($_SESSION["username"])){
 
 ?>
 
-    <div class="column is-11">
-        <h1 class="title is-1">
-            Welcome to Admin
-        </h1>
-        <h2 class="subtitle is-2">
-            <?php echo $user_firstname;?>
-        </h2>
-    </div>
-    <div class="container p-5">
-    </div>
-</div> <!-- Final div for columns -->
-                        
-                        
 
+<main class="column">
+        <div class="level">
+            <div class="level-left">
+                <div class="level-item">
+                    <div class="title">Dashboard</div>
+                </div>
+            </div>
             
-
-
-                <!-- Page Heading -->
-                <!-- <div class="row">
-                    <div class="is-11">
-                        
-                        
+        </div>
+        <div class="columns is-multiline">
+            <div class="column">
+                <div class="box">
+                    <div class="heading">Top Seller Total</div>
+                    <div class="title">56,950</div>
+                    <div class="level">
+                        <div class="level-item">
+                            <div class="">
+                                <div class="heading">Sales $</div>
+                                <div class="title is-5">250,000</div>
+                            </div>
+                        </div>
+                        <div class="level-item">
+                            <div class="">
+                            <div class="heading">Overall $</div>
+                            <div class="title is-5">750,000</div>
+                            </div>
+                        </div>
+                        <div class="level-item">
+                            <div class="">
+                            <div class="heading">Sales %</div>
+                            <div class="title is-5">25%</div>
+                            </div>
+                        </div>
                     </div>
-                </div> -->
-                <!-- /.row -->
+                </div>
+            </div>
+            <div class="column">
+            <div class="box">
+                <div class="heading">Revenue / Expenses</div>
+                <div class="title">55% / 45%</div>
+                <div class="level">
+                <div class="level-item">
+                    <div class="">
+                    <div class="heading">Rev Prod $</div>
+                    <div class="title is-5">30%</div>
+                    </div>
+                </div>
+                <div class="level-item">
+                    <div class="">
+                    <div class="heading">Rev Serv $</div>
+                    <div class="title is-5">25%</div>
+                    </div>
+                </div>
+                <div class="level-item">
+                    <div class="">
+                    <div class="heading">Exp %</div>
+                    <div class="title is-5">45%</div>
+                    </div>
+                </div>
+                </div>
+            </div>
+            </div>
+            <div class="column">
+            <div class="box">
+                <div class="heading">Feedback Activity</div>
+                <div class="title">78% &uarr;</div>
+                <div class="level">
+                <div class="level-item">
+                    <div class="">
+                    <div class="heading">Positive</div>
+                    <div class="title is-5">1560</div>
+                    </div>
+                </div>
+                <div class="level-item">
+                    <div class="">
+                    <div class="heading">Negative</div>
+                    <div class="title is-5">368</div>
+                    </div>
+                </div>
+                <div class="level-item">
+                    <div class="">
+                    <div class="heading">Pos/Neg %</div>
+                    <div class="title is-5">77% / 23%</div>
+                    </div>
+                </div>
+                </div>
+            </div>
+            </div>
+            <div class="column">
+            <div class="box">
+                <div class="heading">Orders / Returns</div>
+                <div class="title">75% / 25%</div>
+                <div class="level">
+                <div class="level-item">
+                    <div class="">
+                    <div class="heading">Orders $</div>
+                    <div class="title is-5">425,000</div>
+                    </div>
+                </div>
+                <div class="level-item">
+                    <div class="">
+                    <div class="heading">Returns $</div>
+                    <div class="title is-5">106,250</div>
+                    </div>
+                </div>
+                <div class="level-item">
+                    <div class="">
+                    <div class="heading">Success %</div>
+                    <div class="title is-5">+ 28,5%</div>
+                    </div>
+                </div>
+                </div>
+            </div>
+            </div>
+        </div>
 
-            <!-- </div> -->
-            <!-- /.container-fluid -->
+        <!-- <div class="columns is-multiline">
+            <div class="column is-6">
+                <div class="panel">
+                    <p class="panel-heading">
+                    Expenses: Daily - $700
+                    </p>
+                    <div class="panel-block">
+                        <figure class="image is-16x9">
+                            <img src="https://placehold.it/1280x720">
+                        </figure>
+                    </div>
+                </div>
+            </div>
+            <div class="column is-6">
+                <div class="panel">
+                    <p class="panel-heading">
+                    Refunds: Daily - $200
+                    </p>
+                    <div class="panel-block">
+                        <figure class="image is-16x9">
+                            <img src="https://placehold.it/1280x720">
+                        </figure>
+                    </div>
+                </div>
+            </div>
+            <div class="column is-6">
+                <div class="panel">
+                    <p class="panel-heading">
+                    Something
+                    </p>
+                    <div class="panel-block">
+                        <figure class="image is-16x9">
+                            <img src="https://placehold.it/1280x720">
+                        </figure>
+                    </div>
+                </div>
+            </div>
+            <div class="column is-6">
+                <div class="panel">
+                    <p class="panel-heading">
+                    Something Else
+                    </p>
+                    <div class="panel-block">
+                        <figure class="image is-16x9">
+                            <img src="https://placehold.it/1280x720">
+                        </figure>
+                    </div>
+                </div>
+            </div>
+        </div> -->
 
-        <!-- </div> -->
-        <!-- /#columns admin-nav -->
+        
+    </main>
+</div> <!-- Final div for columns -->
+
 
         <?php include "includes/admin_footer.php";?>  
